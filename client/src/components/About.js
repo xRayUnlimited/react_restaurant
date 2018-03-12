ea772df9c4c430f370bc1073e209b85b4b9ca5ea
Ray Suyoto <ray.suy@icloud.com>
2018-03-12 16:55:43 -0600

    test update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const About = () => (
+  <div>
   <h1>About</h1>
+  <h3>This is all a figment of your imagination.</h3>
+  </div>
 )
 
 export default About
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1>Home</h1>
-  <h2>Welcome to the React Restaurant</h2>
+    <div class="top">
+      <h2>Welcome to the React Restaurant</h2>
+    </div>
   </div>
 )
 
